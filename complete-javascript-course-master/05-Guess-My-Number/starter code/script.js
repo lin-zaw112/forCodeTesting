1436736141312a0c9247f6ba370cdabeb04b747c
linzaw112 <minotosenpai@gmail.com>
2021-09-30 12:11:14 +0630

    guess my Number games completed

--- complete-javascript-course-master/05-Guess-My-Number/starter code/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter code/script.js
@@ -8,62 +8,25 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-        document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber;
 
 //AGAIN Button
 document.querySelector(".again").addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector('.number').textContent = "?";
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = ' ';
     if (highScore < score) {
         highScore = Number(document.querySelector('.score').textContent);
+        score = 20;
         document.querySelector('.highscore').textContent = highScore;
+        document.querySelector('.score').textContent = score;
+    } else if (score >= highScore) {
         score = 20;
         document.querySelector('.score').textContent = score;
-    } else {
-        score = 20;
-        document.querySelector('.score').textContent = score;
-    }
-    if (!guess) {
-        document.querySelector('.message').textContent = "Please add Number.Cannot find any Number";
-    }
-    // when  input Number is out of 1 to 20
-    else if (guess < 1 && guess > 20) {
-        document.querySelector('.message').textContent = "Number is between 1 and 20";
-    }
-    // when   Player is Win 
-    else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number';
-        document.querySelector('body').style.backgroundColor = 'green';
-        document.querySelector('.number').style.width = '30rem';
-    }
-    // when input Number is Too low 
-    else if (guess < secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = 'Too low';
-            score--;
-            document.querySelector('.score').textContent = score;
-        }
-        // When Player is lose
-        else {
-            document.querySelector('.message').textContent = 'lose the Game';
-            document.querySelector('.score').textContent = "0";
-        }
-    }
-    // when input Number is Too Height
-    else if (guess > secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = 'Too Height';
-            score--;
-            document.querySelector('.score').textContent = score;
-        }
-        // When Player is lose
-        else {
-            document.querySelector('.message').textContent = 'lose the Game';
-            document.querySelector('.score').textContent = "0";
-        }
-    }
+    };
 });
 //Check Button
 document.querySelector('.check').addEventListener('click', function() {
