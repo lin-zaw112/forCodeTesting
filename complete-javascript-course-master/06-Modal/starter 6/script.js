f6d7e00e8b4284a95277351febc4101a30022b8d
linzaw112 <minotosenpai@gmail.com>
2021-10-04 10:26:19 +0630

    Modal window

--- complete-javascript-course-master/06-Modal/starter 6/script.js
+++ complete-javascript-course-master/06-Modal/starter 6/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    modal.classList.add('hidden');
+};
+
+//for Open window
+for (let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', openModal);
+
+
+// for Close Window
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
