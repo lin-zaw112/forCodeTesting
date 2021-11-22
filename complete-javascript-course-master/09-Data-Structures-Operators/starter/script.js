25fc531f7fc2c44ffc865efe93d1e2d47e12b001
lin-zaw112 <minotosenpai@gmail.com>
2021-11-22 10:15:46 +0630

    last Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -5,48 +5,50 @@
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // // Data needed for first part of the section
-
-const restaurant = {
-  restaurantname: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 1,
-    time = '20:20',
-    address = 'Mogok',
-  }) {
-    // console.log(
-    //   `Order Receive ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`
-    // );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicus pasta with ${ing1},${ing2} and ${ing3}`);
-  },
-  openingHours: {
+const openingHours = {
     thu: {
-      open: 12,
-      close: 22,
+        open: 12,
+        close: 22,
     },
     fri: {
-      open: 11,
-      close: 23,
+        open: 11,
+        close: 23,
     },
     sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+        open: 0, // Open 24 hours
+        close: 24,
     },
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient, otherIngredients);
-  },
+};
+const restaurant = {
+    restaurantname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    openingHours,
+    orderDelivery({
+        starterIndex = 1,
+        mainIndex = 1,
+        time = '20:20',
+        address = 'Mogok',
+    }) {
+        console.log(
+            `Order Receive ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`
+        );
+    },
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your delicus pasta with ${ing1},${ing2} and ${ing3}`);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient, otherIngredients);
+    },
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+restaurant.order(1, 0);
 // const orderObject = restaurant.orderDelivery({
 //   time: '20:30',
 //   address: 'Bogo',
@@ -175,11 +177,11 @@ const restaurant = {
 
 // Coding Challenge #1
 
-/* 
+/*
  We're building a football betting app (soccer for my American friends 😅)!
- 
+
  Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
- 
+
  1. Create one player array for each team (variables 'players1' and 'players2')
  2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
  3. Create an array 'allPlayers' containing all players of both teams (22 players)
@@ -187,65 +189,71 @@ const restaurant = {
  5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
  6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
  7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
- 
+
  TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
- 
+
  GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-const players1 = game.players[0];
-const players2 = game.players[1];
-const [gk, ...fieldPlayers] = [...game.players[0]];
-const allPlayers = [...players1, ...players2];
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-const { team1, x: draw, team2 } = game.odds;
-const printGoals = function (playerName) {
-  let sum = 0;
-  for (let i = 0; i < game.scored.length; i++) {
-    if (playerName === game.scored[i]) {
-      sum++;
-    }
-  }
-  console.log(`${playerName} ${sum}`);
-};
-printGoals('Hummels');
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// const [players1, players2] = game.players;
+// const [gk, ...fieldPlayers] = players1;
+// const allPlayers = [...players1, ...players2];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// const printGoals = function (playerName) {
+//   let sum = 0;
+//   for (let i = 0; i < game.scored.length; i++) {
+//     if (playerName === game.scored[i]) {
+//       sum++;
+//     }
+//   }
+//   console.log(`${playerName} ${sum}`);
+// };
+// printGoals('Hummels');
+// const printGoals =function(...players) {
+//   console.log(`${players.length} goals ware score`);
+// }
+// printGoals(...game.scored);
+// team1 < team2 && console.log(`team1 winner`);
