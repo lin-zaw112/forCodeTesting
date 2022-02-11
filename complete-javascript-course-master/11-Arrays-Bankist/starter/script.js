c00324e80302509604f605de5cb6bcb0bcc57b35
lin-zaw112 <minotosenpai@gmail.com>
2022-02-11 14:02:06 +0630

    section 11

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -38,7 +38,7 @@ const accounts = [account1, account2, account3, account4];
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
+const f = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
@@ -60,17 +60,138 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-
+const displayMovents = movements => {
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+          <div class="movements__row">
+              <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+              <div class="movements__date">3 days ago</div>
+              <div class="movements__value">${mov}€</div>
+          </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovents(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// // console.log(arr.slice(-1));
+// // console.log(arr.slice(2, -1));
+//  * console.log(arr.slice());
+//  * console.log([...arr]);
+// * const newArr = arr.concat(arr2);
+
+// !console.log(arr.slice(2, 4));
+// !console.log(arr.splice(2, 4));
+// // console.log(arr);
+
+// // console.log(arr);
+// // arr.reverse();
+// arr2.reverse();
+// // console.log(newArr);
+// const letter = newArr.join('-');
+// console.log(letter);
+// console.log(...arr, ...arr2);
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
+// const movementUsd = movements.map(mov => Math.trunc(mov * eurToUsd));
+// console.log(movementUsd);
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} You Deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} You Withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// movements.forEach((mov, index, array) => {
+//   if (mov > 0) {
+//     console.log(`movement ${index + 1} You Deposited ${mov}`);
+//   } else {
+//     console.log(`movement ${index + 1} You Withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+/////////////////////////////////////////////////
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach((val, i, map) => {
+//   console.log(`${i} : ${val}`);
+// });
+
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach((val, _, set) => {
+//   console.log(`${val} : ${val}`);
+// });
+/////////////////////////////////////////////////
+
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+// */
+
+// const checkDogs = (dogsJulia, dogsKate) => {
+//   const dogsJuliaCorrected = dogsJulia;
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-1);
+//   const arrofdogs = [...dogsJuliaCorrected, ...dogsKate];
+//   // ! const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   arrofdogs.forEach((dogAge, i) => {
+//     const ageTest =
+//       dogAge >= 3 ? `an adult, and is ${dogAge} years old` : 'still a puppy 🐶';
+//     console.log(`Dog number ${i + 1} is ${ageTest}`);
+//   });
+// };
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+const user = 'Steven Thomas Williams';
+const userName = user
+  .toLowerCase()
+  .split(' ')
+  .map(name => name[0])
+  .join('');
+console.log(userName);
